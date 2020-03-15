350130e25986f2a5ce8cd4f090aabb43b76d9752
WenXiaowei <xiaowei.wen1997@gmail.com>
2020-03-15 21:33:55 +0100

    WIP implementazione CardView espandibile dell'elenco delle orgs.

--- app/src/main/java/com/vartmp7/stalker/ui/home/HomeFragment.java
+++ app/src/main/java/com/vartmp7/stalker/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.vartmp7.stalker.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,18 +12,23 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.vartmp7.stalker.R;
 
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private final static String TAG = "com.vartmp7.stalker.ui.home.HomeFragment";
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    private HomeViewModel homeViewModel;
+    private String id_newOrg;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle b) {
+
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_status, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -32,5 +38,14 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle b) {
+        super.onViewCreated(view, b);
+        if (b != null) {
+            id_newOrg = b.getString("ID_ORG");
+            Log.d(TAG, "onCreateView: " + id_newOrg);
+        }
     }
 }
--- app/src/main/java/com/vartmp7/stalker/ui/organizations/OrganizationAdapter.java
+++ app/src/main/java/com/vartmp7/stalker/ui/organizations/OrganizationAdapter.java
@@ -1,35 +1,35 @@
 package com.vartmp7.stalker.ui.organizations;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.internal.$Gson$Preconditions;
-import com.vartmp7.stalker.MainActivity;
 import com.vartmp7.stalker.R;
 import com.vartmp7.stalker.component.gsonbeans.Organizzazione;
-import com.vartmp7.stalker.ui.home.HomeFragment;
 
 import java.util.List;
 
-public class OrganizationAdapter  extends RecyclerView.Adapter<OrganizationAdapter.ViewHolder> {
+public class OrganizationAdapter extends RecyclerView.Adapter<OrganizationAdapter.ViewHolder> {
     private List<Organizzazione> listaOrganizzazione;
-    public OrganizationAdapter(List<Organizzazione> list){
-            listaOrganizzazione= list;
-    }
+    private Context context;
+    private static int currentPosition = -1;
 
+    public OrganizationAdapter(Context context, List<Organizzazione> list) {
+        listaOrganizzazione = list;
+        this.context = context;
+    }
 
 
     @NonNull
@@ -37,22 +37,18 @@ public class OrganizationAdapter  extends RecyclerView.Adapter<OrganizationAdapt
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_organization_list, parent, false);
-        ViewHolder viewHolder=  new ViewHolder(view);
-        viewHolder.tvTrackMe.setOnClickListener(new View.OnClickListener() {
+        ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.btnTrackMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int position = viewHolder.getAdapterPosition();
+                Organizzazione org = listaOrganizzazione.get(position);
 
                 NavController navController = Navigation.findNavController(parent);
                 Bundle b = new Bundle();
-//                aggiungere dati in b
-
+                b.putInt("ID_ORG", Integer.parseInt(org.getId()));
+                navController.saveState();
                 navController.navigate(R.id.navigation_status, b);
-
-                int position = viewHolder.getAdapterPosition();
-
-
-                Organizzazione org = listaOrganizzazione.get(position);
-                Snackbar.make(parent, org.getName(), Snackbar.LENGTH_SHORT).show();
             }
         });
         return viewHolder;
@@ -60,10 +56,36 @@ public class OrganizationAdapter  extends RecyclerView.Adapter<OrganizationAdapt
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            Organizzazione org = listaOrganizzazione.get(position);
+        Organizzazione org = listaOrganizzazione.get(position);
 
-            holder.nomeOrganizzazione.setText(org.getName());
-            holder.tipoOrganizzazione.setText(org.getType());
+        holder.nomeOrganizzazione.setText(org.getName() + " " + org.getId());
+        holder.tipoOrganizzazione.setText("tipo: " + org.getType());
+        holder.tvIndirizzo.setText(org.getOrgInfo());
+
+
+        if (currentPosition == position && holder.llHidingInfo.getVisibility()==View.INVISIBLE) {
+            Animation slideDown = AnimationUtils.loadAnimation(context, R.anim.slide_down_animation);
+            holder.llHidingInfo.setVisibility(View.VISIBLE);
+            holder.tvIndirizzo.setVisibility(View.VISIBLE);
+            holder.llHidingInfo.startAnimation(slideDown);
+        }else if(currentPosition==position && holder.llHidingInfo.getVisibility()==View.VISIBLE){
+            Animation closing = AnimationUtils.loadAnimation(context, R.anim.closing_animation);
+            holder.llHidingInfo.setVisibility(View.INVISIBLE);
+            holder.tvIndirizzo.setVisibility(View.INVISIBLE);
+            holder.llHidingInfo.startAnimation(closing);
+            currentPosition=-1;
+        }
+        holder.nomeOrganizzazione.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //getting the position of the item to expand it
+                currentPosition = position;
+
+                //reloding the list
+                notifyDataSetChanged();
+            }
+        });
 //            holder.numeroLuoghi = org.get
     }
 
@@ -72,16 +94,21 @@ public class OrganizationAdapter  extends RecyclerView.Adapter<OrganizationAdapt
         return listaOrganizzazione.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder {
         TextView nomeOrganizzazione;
-        TextView tipoOrganizzazione;
-        Button tvTrackMe;
+        TextView tipoOrganizzazione, tvIndirizzo;
+        Button btnTrackMe;
+        LinearLayout llHidingInfo;
+
         public ViewHolder(@NonNull View itemView) {
             // dati dell'organizzazione
             super(itemView);
             nomeOrganizzazione = itemView.findViewById(R.id.tvNomeOrganizzazione);
             tipoOrganizzazione = itemView.findViewById(R.id.tvTipoOrganizzazione);
-            tvTrackMe = itemView.findViewById(R.id.btnTrackMe);
+            btnTrackMe = itemView.findViewById(R.id.btnTrackMe);
+            tvIndirizzo = itemView.findViewById(R.id.tvIndirizzo);
+            llHidingInfo = itemView.findViewById(R.id.llHidingInformation);
+
         }
     }
 
