28d35f2d726d960163af5d3bd7add233a46a6e08
Lorenzo <taschin1998@gmail.com>
2020-03-16 16:14:44 +0100

    aggiunti signIn e signUp

--- app/src/main/java/com/vartmp7/stalker/LoginActivitity.java
+++ app/src/main/java/com/vartmp7/stalker/LoginActivitity.java
@@ -1,9 +1,15 @@
 package com.vartmp7.stalker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,6 +29,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
@@ -39,7 +46,11 @@ public class LoginActivitity extends BaseActivity implements View.OnClickListene
     private CallbackManager callbackManager;
     private FirebaseAuth mAuth;
 
+    private Button signUpButton;
+    private Button signInButton;
+
     private GoogleSignInClient mGoogleSignInClient;
+
 
     //    keytool -exportcert -alias YOUR_RELEASE_KEY_ALIAS -keystore YOUR_RELEASE_KEY_PATH | openssl sha1 -binary | openssl base64
     @Override
@@ -55,6 +66,12 @@ public class LoginActivitity extends BaseActivity implements View.OnClickListene
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUpButton = (Button)findViewById(R.id.btn_signUp);
+        signUpButton.setOnClickListener(this);
+
+        signInButton = (Button)findViewById(R.id.btn_signIn);
+        signInButton.setOnClickListener(this);
 
 // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -162,10 +179,111 @@ public class LoginActivitity extends BaseActivity implements View.OnClickListene
         }
     }
 
+    public void showSignUpDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setTitle(R.string.sign_up);
+        builder.setMessage("Iscriviti");
+        builder.setView(inflater.inflate(R.layout.dialog_login, null));
+        builder.setPositiveButton(getString(R.string.conferma), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Dialog d = (Dialog) dialog;
+                EditText etUsername = d.findViewById(R.id.etUsername);
+                EditText etPassword = d.findViewById(R.id.etPassword);
+
+                String email = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                        task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivitity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                );
+            }
+        });
+
+        builder.setNegativeButton(R.string.annulla, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+
+    }
+
+    private void showSignInDialog()  {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setTitle(R.string.sign_up);
+        builder.setMessage("Iscriviti");
+        builder.setView(inflater.inflate(R.layout.dialog_login, null));
+        builder.setPositiveButton(getString(R.string.conferma), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Dialog d = (Dialog) dialog;
+                EditText etUsername = d.findViewById(R.id.etUsername);
+                EditText etPassword = d.findViewById(R.id.etPassword);
+
+                String email = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+                        task-> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivitity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            // ...
+                        }
+                );
+
+
+            }
+        });
+
+        builder.setNegativeButton(R.string.annulla, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+
+    }
+
+
+
     private void googleSignIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+
 
     private void googleSignOut() {
         mGoogleSignInClient.signOut()
@@ -186,7 +304,17 @@ public class LoginActivitity extends BaseActivity implements View.OnClickListene
             case R.id.btnProcediSenzaAuth:
                 startActivity(new Intent(LoginActivitity.this, MainActivity.class));
                 break;
+            case R.id.btn_signUp:
+                Log.e(TAG,"cliccato btn signUp");
+                showSignUpDialog();
+                break;
+            case R.id.btn_signIn:
+                showSignInDialog();
+                break;
         }
     }
+
+
+
 
 }
